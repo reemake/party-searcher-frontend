e306fe876a94c3cded1d2377bb1625af1beb8367
temkarus0070 <pretkovartyom@gmail.com>
2022-02-13 13:23:54 +0300

    update server.js

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/event-teammates-search-frontend'));
+app.use(express.static('./dist/event-teammates-search-app'));
 
 app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/event-teammates-search-frontend/'}),
+  res.sendFile('index.html', {root: 'dist/event-teammates-search-app/'}),
 );
 
 
