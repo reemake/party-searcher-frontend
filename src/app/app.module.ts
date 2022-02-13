f9008ea8426a452bb18e3f0bf440e4579f3bf0d6
temkarus0070 <pretkovartyom@gmail.com>
2022-02-13 13:15:45 +0300

    remove trash

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { MapComponent } from './components/map/map.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MapComponent
   ],
   imports: [
     BrowserModule
