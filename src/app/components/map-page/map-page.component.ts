20b2da599e0eba2298e10cd7b1b7bf2a87d0578e
temkarus0070 <pretkovartyom@gmail.com>
2022-02-13 16:01:43 +0300

    add map

---
+++ src/app/components/map-page/map-page.component.ts
@@ -0,0 +1,61 @@
+import {AfterViewInit, ChangeDetectorRef, Component, EventEmitter, Input, NgZone, Output} from '@angular/core';
+import {Map, View} from 'ol';
+import {Coordinate} from 'ol/coordinate';
+import {defaults as DefaultControls, ScaleLine} from 'ol/control';
+import proj4 from 'proj4';
+import Projection from 'ol/proj/Projection';
+import {register} from 'ol/proj/proj4';
+import {get as GetProjection} from 'ol/proj'
+import {Extent} from 'ol/extent';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+
+@Component({
+  selector: 'app-map-page',
+  templateUrl: './map-page.component.html',
+  styleUrls: ['./map-page.component.css']
+})
+export class MapPageComponent implements AfterViewInit {
+
+  @Input() center: Coordinate | undefined;
+  @Input() zoom: number | undefined;
+  view: View | undefined;
+  projection: Projection | undefined;
+  extent: Extent = [-20026376.39, -20048966.10,
+    20026376.39, 20048966.10];
+  Map: Map | undefined;
+  @Output() mapReady = new EventEmitter<Map>();
+
+  constructor(private zone: NgZone, private cd: ChangeDetectorRef) {
+  }
+
+
+  ngAfterViewInit(): void {
+    if (!this.Map) {
+      this.zone.runOutsideAngular(() => this.initMap())
+    }
+    setTimeout(() => this.mapReady.emit(this.Map));
+  }
+
+  private initMap(): void {
+    proj4.defs("EPSG:3857", "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs");
+    register(proj4)
+    this.projection = GetProjection('EPSG:3857');
+    this.projection.setExtent(this.extent);
+    this.view = new View({
+      center: this.center,
+      zoom: this.zoom,
+      projection: this.projection,
+    });
+    this.Map = new Map({
+      layers: [new TileLayer({
+        source: new OSM({})
+      })],
+      target: 'map',
+      view: this.view,
+      controls: DefaultControls().extend([
+        new ScaleLine({}),
+      ]),
+    });
+  }
+}
