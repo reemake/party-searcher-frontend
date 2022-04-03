bc52557d79acb44276fe1dab2a988fe2ffe3919e
Dmtr1i <dmitry1klimenko@mail.ru>
2022-04-03 11:12:18 +0300

    working on friend requests

--- src/app/components/pages/friends/friends.component.ts
+++ src/app/components/pages/friends/friends.component.ts
@@ -3,6 +3,7 @@ import {UserService} from './user.service'
 import { User } from './user';
 import {HttpClient} from "@angular/common/http";
 import { BACKEND_URL } from 'src/app/app.module';
+import { Relationship } from './Relationship';
 
 @Component({
   selector: 'app-friends',
@@ -13,7 +14,17 @@ export class FriendsComponent implements OnInit {
 
   findCheck: boolean = false;
   users: User[];
+  requests: Relationship[];
+  requestsCheck: boolean = false;
+
   constructor(private userService: UserService, private httpClient: HttpClient) {
+    this.userService.getRequests().subscribe((data: User[]) => {
+      this.requests = data;
+      console.log("waiting requests");
+      console.log(data);
+      this.requestsCheck = true;
+    });
+    console.log(this.requests);
   }
 
   ngOnInit(): void {
@@ -26,7 +37,6 @@ export class FriendsComponent implements OnInit {
       this.hideFields();
       this.userService.getUsers((<HTMLInputElement>field).value).subscribe((data: User[]) => {
         this.users = data;
-        console.log(this.users);
         this.findCheck = true;
       })
     } else {
@@ -55,10 +65,8 @@ export class FriendsComponent implements OnInit {
 
   clickFriendButton(event: any): void {
     var friendLogin: string = (<HTMLInputElement>event.path[0]).id;
-    console.log(friendLogin);
     var data = { "friendName": friendLogin};
     console.log("sending data");
-    console.log(data);
     this.httpClient.post<any>(BACKEND_URL + "/api/requestFriend", null, {headers: data}).subscribe(e=> {});
   }
 
