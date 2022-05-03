fe88f233a38e5ef21af39a7f313c24bc0f5dae44
temkarus0070 <pretkovartyom@gmail.com>
2022-05-03 17:58:27 +0300

    complete event review

--- src/app/components/profile/complaints-panel/complaints-panel.component.ts
+++ src/app/components/profile/complaints-panel/complaints-panel.component.ts
@@ -3,6 +3,7 @@ import {ComplaintService} from "../../../services/complaint.service";
 import {Complaint} from "../../../entity/Event/Complaint";
 import {MatDialog} from "@angular/material/dialog";
 import {WarningCreateDialogComponent} from "../../warning-create-dialog/warning-create-dialog.component";
+import {ComplaintStatus} from "../../../entity/Event/ComplaintStatus";
 
 @Component({
   selector: 'app-complaints-panel',
@@ -11,15 +12,44 @@ import {WarningCreateDialogComponent} from "../../warning-create-dialog/warning-
 })
 export class ComplaintsPanelComponent implements OnInit {
   public complaints: Complaint[] = [];
+  public currentComplaints: Complaint[] = [];
   public statusesOfOperations = new WeakMap<any, string>();
+  public complaintsStatusesMap = new Map<ComplaintStatus, string>();
+  public currentStatus = ComplaintStatus.ACCEPTED;
+  public items: any[] = [];
 
   constructor(private complaintsService: ComplaintService, private dialog: MatDialog) {
+
+
+    this.complaintsStatusesMap = this.complaintsStatusesMap.set(ComplaintStatus.REJECTED, "Отклонены");
+    this.complaintsStatusesMap = this.complaintsStatusesMap.set(ComplaintStatus.ACCEPTED, "Обработаны");
+    this.complaintsStatusesMap = this.complaintsStatusesMap.set(ComplaintStatus.PENDING, "Не обработаны");
+    this.setItems();
     this.complaintsService.findComplaintsAssignedToMeToResolve().subscribe(complaints => {
       this.complaints = complaints;
+
+      this.currentComplaints = this.complaints.filter(e => {
+        return e.status === this.currentStatus;
+      })
+    })
+  }
+
+  setItems() {
+    var array: any[] = [];
+    for (let status of this.complaintsStatusesMap.keys()) {
+      array.push({id: status, name: this.complaintsStatusesMap.get(status)});
+    }
+    this.items = array;
+  }
+
+  changeFilter() {
+    this.currentComplaints = this.complaints.filter(e => {
+      return e.status === this.currentStatus;
     })
   }
 
   ngOnInit(): void {
+
   }
 
   sendWarning(complaint: Complaint) {
