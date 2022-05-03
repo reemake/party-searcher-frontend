3128c681eb6125fcd0e02eda8c06140489c127c6
temkarus0070 <pretkovartyom@gmail.com>
2022-05-03 21:31:43 +0300

    repair  notifications

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {AuthenticationService} from 'src/app/services/auth/authentication.service';
 import {InviteService} from './invite.service';
+import {NotificationService} from "../../services/notification.service";
 
 @Component({
   selector: 'app-header',
@@ -12,17 +13,27 @@ export class HeaderComponent implements OnInit {
 
   public invitesCheck: boolean = false;
 
-  constructor(private _router: Router, public _authService: AuthenticationService, private inviteService: InviteService) {
+  constructor(private _router: Router, public _authService: AuthenticationService, private inviteService: InviteService, private notificationService: NotificationService) {
     if (localStorage.getItem("token")) {
       inviteService.isInvated().subscribe((data) => {
         this.invitesCheck = data;
       });
+      this.notificationService.hasNew().subscribe(e => {
+        if (e) {
+          this.invitesCheck = e;
+        }
+      })
     }
     setInterval(() => {
       if (localStorage.getItem("token")) {
         inviteService.isInvated().subscribe((data) => {
           this.invitesCheck = data;
         });
+        this.notificationService.hasNew().subscribe(e => {
+          if (e) {
+            this.invitesCheck = e;
+          }
+        })
       }
     }, 60000);
   }
